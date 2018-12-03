303073982af018630f80ab98e16114912cc60f81
Oliver Anteros <rift95@live.se>
2018-12-03 23:42:38 +0100

    renderer now supports tables, emoji & some github specific stuff

--- github-readme.js
+++ github-readme.js
@@ -192,6 +192,10 @@ window.customElements.define('github-readme', class extends HTMLElement {
         }, 33);
         const run = () => { 
             this.converter = new showdown.Converter();
+            this.converter.setOption('tables', true);
+            this.converter.setOption('ghCodeBlocks', true);
+            this.converter.setOption('ghMentions', true);
+            this.converter.setOption('emoji', true);
             this.history = window.History.createMemoryHistory();
             this.history.listen((location, action) => {
               // location is an object like window.location
